7dd4dd8d91f95f6ac2a846fe9657eb2fd7a240af
カブラン <cablan.houssou@icloud.com>
2021-12-05 22:04:38 +0100

    Store message components

---
+++ script.js
@@ -0,0 +1,5 @@
+const messageElements = [
+    ['You','I','We','Humanity'],
+    ['Will','Must', 'Should'],
+    ['Be better today','Stop being afraid of the Future. Everything will be alright.','Not be afraid of life. Believe that life is worth living','Choose to be Happy', 'Choose kindness over Conflict']
+];
