e5c79e8903598709692f69033cee1ed85287266a
カブラン <cablan.houssou@icloud.com>
2021-12-05 22:05:45 +0100

    Make a function to create the message

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const messageElements = [
     ['Will','Must', 'Should'],
     ['Be better today','Stop being afraid of the Future. Everything will be alright.','Not be afraid of life. Believe that life is worth living','Choose to be Happy', 'Choose kindness over Conflict']
 ];
+
+const makeRandomMessage = () => {
+    let randomMessage = [];
+    for ( let messagePiece of messageElements ){
+        let randomIndex = Math.floor(Math.random() * messagePiece.length);
+        randomMessage.push(messagePiece[randomIndex]);
+    }
+    return randomMessage;
+};
